e095b90509b767b358680a017c8d2a48d3c4cda1
tshi04 <gpview@life-tp.com>
2019-08-09 11:23:29 -0400

    update

--- run.py
+++
@@ -1,6 +0,0 @@
-
-
-# from LeafNATS.playground.nats.pointer_generator_network.main import *
-# from LeafNATS.playground.nats.headline_summary_multiTask.main import *
-# from LeafNATS.playground.nats.pointer_generator_network_trans.main import *
-from LeafNATS.playground.nats.headline2_summary2_app.main_app import *
